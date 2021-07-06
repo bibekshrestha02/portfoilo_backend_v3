6e9f50377f67a6460e7d417d18037d393766cdd6
Bibek Shrestha <shresthabbks@gmail.com>
2021-07-06 13:28:56 +0545

    created db files

---
+++ src/startup/db.ts
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+function db() {
+  let db: string;
+  if (process.env.NODE_ENV === 'development') {
+    db = process.env.mongo_local as string;
+  } else {
+    db = process.env.mongo_server as string;
+  }
+  mongoose
+    .connect(db, {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log('connected to database'))
+    .catch((err) => console.log(err));
+}
+export default db;
