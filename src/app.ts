5e8b6e26ce8b29b35fba546a3c0719ccb105788b
Bibek Shrestha <shresthabbks@gmail.com>
2021-07-07 14:34:11 +0545

    refactor app pages

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,18 @@
-import express, { Application, Response, Request } from 'express';
+import express, { Application } from 'express';
 import { json } from 'body-parser';
 import dotenv from 'dotenv';
-import db from './startup/db';
+import run from './startup/db';
 import routes from './startup/routes';
 const app: Application = express();
 dotenv.config({ path: '.env' });
 app.use(json());
 
 routes(app);
-db();
+run();
 
 let port = process.env.PORT || 5000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log('Server started');
 });
+
+export default server;
