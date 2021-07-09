ef63a7e4e2957be27fc6792147d7877b6c9e8a45
Bibek Shrestha <shresthabbks@gmail.com>
2021-07-09 12:37:28 +0545

    Inital Testing Doc

--- src/tests/integrations/route/initalRoute.test.ts
+++ src/tests/integrations/route/initalRoute.test.ts
@@ -1,3 +1,24 @@
 import Request from 'supertest';
+import app from '../../../app';
+import mongoose from 'mongoose';
+import { testRun } from '../../../startup/db';
+import UserModel from '../../../model/usersModel';
+describe('Inital Route: /api/v1/', () => {
+  beforeAll(() => {
+    testRun();
+  });
+  afterEach(async () => {
+    await UserModel.deleteMany({});
+  });
+  afterAll(async () => {
+    await mongoose.connection.close();
+  });
 
-describe('Inital Route: /api/v1/', () => {});
+  it('should return status 200', async () => {
+    const users = await UserModel.find({});
+    await UserModel.create({ name: 'Bibek' });
+    console.log(users);
+    const res = await Request(app).get('/api/v1/');
+    expect(res.status).toBe(200);
+  });
+});
