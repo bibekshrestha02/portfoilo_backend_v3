a7b9c7f5e6f3d49efa9f21dcd63484b056a5ed12
Bibek Shrestha <shresthabbks@gmail.com>
2021-07-09 12:36:59 +0545

    Created test db connection

--- src/startup/db.ts
+++ src/startup/db.ts
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-function run() {
+const run = () => {
   let db: string;
   if (process.env.NODE_ENV === 'development') {
     db = process.env.mongo_local as string;
@@ -14,5 +14,18 @@ function run() {
     })
     .then(() => console.log('connected to database'))
     .catch((err) => console.log(err));
-}
+};
+
+export const testRun = () => {
+  let db: string = process.env.mongo_local_test as string;
+
+  mongoose
+    .connect(db, {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log('connected to database'))
+    .catch((err) => console.log(err));
+};
 export default run;
