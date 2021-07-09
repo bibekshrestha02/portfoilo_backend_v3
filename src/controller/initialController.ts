e532be8ce9aa6b1f40287f582af8312c2bc2739e
Bibek Shrestha <shresthabbks@gmail.com>
2021-07-09 15:52:24 +0545

    Written getInitalController

--- src/controller/initialController.ts
+++ src/controller/initialController.ts
@@ -1,7 +1,38 @@
 import { Response, Request, NextFunction } from 'express';
+import ColorModel from '../model/colorModel';
+import UserModel from '../model/usersModel';
+export async function getInital(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    // Find the document
+    const query = {},
+      update = {
+        isCreate: 'true',
+      },
+      options = { upsert: true, new: true, setDefaultsOnInsert: true };
 
-export function getInital(req: Request, res: Response, next: NextFunction) {
-  res.status(200).json({
-    name: 'Bibek',
-  });
+    // Find the document
+    let user = await UserModel.findOneAndUpdate(
+      query,
+      update,
+      options,
+      async function (error: any, result: any) {
+        if (error) return;
+        if (!result) {
+          await UserModel.create({});
+        }
+      }
+    )
+      .select('name title profileImagePath socialLinks')
+      .populate('socialLinks')
+      .lean();
+    let colors = await ColorModel.find({}).lean();
+
+    res.json({ ...user, colors });
+  } catch (error) {
+    console.log(error);
+  }
 }
