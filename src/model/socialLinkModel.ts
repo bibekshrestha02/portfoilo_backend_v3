f727bb1878056c100b63c5c37c3d463e14d7bf6a
Bibek Shrestha <shresthabbks@gmail.com>
2021-07-11 17:50:58 +0545

    added validation schema

--- src/model/socialLinkModel.ts
+++ src/model/socialLinkModel.ts
@@ -1,14 +1,24 @@
 import { Schema, model } from 'mongoose';
+import * as yup from 'yup';
 const socialLinkSchema = new Schema({
   name: {
     type: String,
+    required: true,
   },
   iconPath: {
+    required: true,
     type: String,
   },
   link: {
     type: String,
+    required: true,
   },
 });
+export const socialLinkValidationSchema = yup.object().shape({
+  name: yup.string().required(),
+  iconPath: yup.string().required(),
+  link: yup.string().required(),
+});
+
 const socialLinkModel = model('SocialLink', socialLinkSchema);
 export default socialLinkModel;
